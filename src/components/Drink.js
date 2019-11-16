d35280c8c10318e11aaf0961ea369efd167d4a17
JenniKate Wallace <jennikate@gmail.com>
2019-11-16 00:00:41 +0000

    data pulling

--- src/components/Drink.js
+++ src/components/Drink.js
@@ -3,12 +3,19 @@ import React from 'react'
 const Drink = ({ drink }) => {
   console.log(drink)
   return (
-    <div className="columns is-vcentered is-multiline">
+    <div className="columns is-vcentered is-multiline flip-card">
       {drink.map((drink, i) => {
         return (
-          <div className="column is-one-quarter" key={i}>
-            <a id={drink.idDrink}>{drink.strDrink}</a>
-            <img src={drink.strDrinkThumb} />
+          <div className="column is-one-quarter flip-card-inner" key={i}>
+            <div className='flip-card-front'>
+              <img src={drink.strDrinkThumb} />
+              <p id={drink.idDrink}>{drink.strDrink}</p>
+            </div>
+            <div className='flip-card-back'>
+              <p className='drink-instructions'>{drink.strInstructions}</p>
+              <p className='drink-classifications'>{drink.strCategory}, {drink.strAlcoholic}</p>
+              <p>Find recipe on <a target='_blank' href={`https://lmgtfy.com/?q=${drink.strDrink}`}>this search.</a></p>
+            </div>
           </div>
         )
       })}
@@ -18,5 +25,3 @@ const Drink = ({ drink }) => {
 }
 
 export default Drink
-
-
